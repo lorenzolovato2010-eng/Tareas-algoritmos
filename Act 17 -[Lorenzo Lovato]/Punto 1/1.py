c03d12a8344642f6a5ebec63bee63ab5645ccc25
lorenzolovato2010-eng <lorenzolovato2010@gmail.com>
2026-06-30 23:26:15 -0300

    Add file via upload
    .

--- Act 17 -[Lorenzo Lovato]/Punto 1/1.py
+++ Act 17 -[Lorenzo Lovato]/Punto 1/1.py
@@ -3,7 +3,7 @@
 lista=[[100,7,85,8], [4,8,56,25], [67,89,23,1], [78,56]]
 Imprimir la lista. Luego fijar con el valor cero todos los elementos mayores a 50
 del primer elemento de "lista";.
-Volver a imprimir la lista.
+Volver a imprimir la lista
 """
 aux=0
 lista=[[100,7,85,8], [4,8,56,25], [67,89,23,1], [78,56]]
