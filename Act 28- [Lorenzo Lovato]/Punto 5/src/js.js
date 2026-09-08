69eaf8c76563ae5888885f653e1575a71106cbd5
lorenzolovato2010-eng <lorenzolovato2010@gmail.com>
2026-09-08 15:49:29 -0300

    Delete Act 28- [Lorenzo Lovato] directory

--- Act 28- [Lorenzo Lovato]/Punto 5/src/js.js
+++
@@ -1,8 +0,0 @@
-/*
-5. Confeccionar una página que muestre un mensaje con la función alert inmediatamente
-después que se cargue.
-*/
-function mostrarAlertaCarga() {
-    alert(
-        "La página se ha cargado completamente.");
-}
